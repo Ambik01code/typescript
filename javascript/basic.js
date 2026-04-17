89cd2c9a2e80d6b289292fc1c5616ca531fa383d
Ambik01code <ambikbanerjee@gmail.com>
2026-04-17 17:49:59 +0530

    Add files via upload

---
+++ javascript/basic.js
@@ -0,0 +1,58 @@
+//let  n= 10;
+//n = 20; // Value can be updated
+// let n = 15; //can not redeclare
+//console.log(n)//
+
+// var n= 5;
+// var n= 7;
+// console.log(n)
+
+// const a= 10;
+// console.log(a)
+
+// let a=23
+
+// function hello(){
+//          let a=12  
+   
+//     {
+//        let a=90
+ 
+//         console.log(a)
+//     }
+    
+     
+// }
+
+
+// hello()
+// console.log(a)
+
+// let a = [1, 2, 3, 4, 5];
+
+// // a.push(12, 13)
+
+// a.pop();
+
+// console.log(a);
+
+
+// let a = [1, 2, 3, 4, 5];
+
+// // a.unshift(12, 23)
+
+// a.shift();
+
+// console.log(a);
+
+
+// let a= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let b=a. splice (2,4,7);
+// console.log(b);
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+a.splice(9, 0, "ji");
+
+console.log(a);
+
